150f8025a8cc9940fd67faea54159fb8758d3764
Mateus Mourão <mateusm@take.net>
2021-07-16 19:44:17 -0300

    add tarefas array with two tasks

---
+++ tarefas.js
@@ -0,0 +1,12 @@
+let tarefas = [
+  {
+    nome: "fazer esse tp",
+    categoria: "estudos",
+    realizada: false
+  },
+  {
+    nome: "jogar valorant",
+    categoria: "lazer",
+    realizada: false
+  }
+]
