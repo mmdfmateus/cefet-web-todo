7477667eb93d603f785e8bb19efb95a1c0a41243
Mateus Mourão <mateusm@take.net>
2021-07-16 21:26:59 -0300

    add ex2 implementation

--- tarefas.js
+++ tarefas.js
@@ -10,3 +10,43 @@ let tarefas = [
     realizada: false
   }
 ]
+
+const insereTarefaNaPagina = (tarefa) => {
+  let listaEl = document.querySelector('#lista-tarefas');
+  
+  const newItem = document.createElement('li');
+  newItem.classList.add('item-tarefa');
+  newItem.innerText = tarefa.nome;
+  
+  listaEl.appendChild(newItem);
+}
+
+const populaTarefasExistentes = () => {
+  let listaEl = document.querySelector('#lista-tarefas');
+  listaEl.innerHTML = '';
+  
+  for(let item of tarefas){
+    console.log(item);
+    insereTarefaNaPagina(item);
+  }
+}
+
+const addButton = document.getElementById('incluir-nova-tarefa');
+addButton.addEventListener('click', (ev) => {
+  const input = document.querySelector('#nova-tarefa-nome');
+  const category = document.querySelector('#nova-tarefa-categoria');
+  
+  const tarefa = {
+    nome: input.value,
+    category: category.value,
+    realizada: false
+  }
+  
+  insereTarefaNaPagina(tarefa);
+  tarefas.push(tarefa);
+
+  input.value = '';
+  input.focus();
+});
+
+populaTarefasExistentes();
